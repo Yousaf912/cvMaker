0dfea6481df7e8fd7bdddc75c4cedbc47823e3e7
Muhammad Yousaf <yousafva9@gmail.com>
2024-12-30 18:23:17 +0500

    updated

--- src/Components/BuildingResumeSession/Experirnce/Experience.jsx
+++ src/Components/BuildingResumeSession/Experirnce/Experience.jsx
@@ -1,29 +1,80 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './Experience.module.css'
 import { IoIosAddCircle } from "react-icons/io";
 import { useState } from 'react';
 import { MdOutlineDeleteOutline } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
 
 
 
 export default function Experience() {
-    const navigate = useNavigate()
-    const [fields, setFields] = useState([]);
-    const addField = () => {
-        setFields([...fields, <InutFields />])
+    const id = localStorage.getItem('userid');
+    const navigate = useNavigate();
+    const [eror, seteror] = useState({});
+
+    useEffect(() => {
+        if (!id) {
+            navigate('/')
+        }
+    }, []);
+
+    const [data, setdata] = useState({
+        title: '',
+        office: '',
+        address: '',
+        startingDate: '',
+        endDate: ''
+    });
+
+    const getdata = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setdata({
+            ...data,
+            [name]: value
+        })
     }
 
-    const deleteField = (i) => {
-        const newFields = [...fields];
-        newFields.splice(i, 1);
-        setFields(newFields);
-    };
+    const add = async () => {
+        const url = import.meta.env.VITE_FETCHING_URL;
+        try {
+            await fetch(`${url}/addexperince/${id}`, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            }).then(async (res) => {
+                const fnal = await res.json();
+                console.log(fnal);
+                if (fnal.message == 'validation eror') {
+                    seteror(fnal.eror.errors)
+                } else {
+                    if (fnal.message == 'data can not be exceed more than 5') {
+                        toast.error(fnal.message)
+                    }
+                    if (fnal.message == 'added') {
+                        toast.success('experience is added')
+                        seteror({});
+                        setdata({
+                            title: '',
+                            office: '',
+                            address: '',
+                            startingDate: '',
+                            endDate: ''
+                        });
 
-    const experience = () => {
-        navigate('/makeResume/skills')
+                    }
+                }
+
+            })
+        } catch (er) {
+            console.log(er);
+        }
     }
-
 
 
     return (
@@ -31,67 +82,47 @@ export default function Experience() {
             <h1>Tell us about your most recent job</h1>
             <h4>We’ll start there and work backward. Tell us your experirnce where do you work </h4>
             <div className=''>
-                <h4>Experince 1</h4>
-                <InutFields />
-            </div>
-            {fields.map((val, i) => {
-                return (
-
-                    <div className='mt-4' key={i}>
-                        <div className='d-flex justify-content-between col-5 mt-5 pt-4'>
-                            <h4>Experience {i + 2}</h4>
-                            <MdOutlineDeleteOutline onClick={() => deleteField(i)} className='fs-1 text-danger' />
-                        </div>
-                        {val}</div>
-                )
-            })}
-            <div onClick={addField} className='d-flex col-2  align-items-center border bg-primary text-white  btn  mt-5 '>
-                Add More
-                <IoIosAddCircle className='fs-2 ms-2 ' />
-
-            </div>
-
-            <div className={`mt-5 ${style.main} text-center  `}>
-                <button onClick={experience} className='btn py-3  px-3 shadow-lg'>
-                    Next to: Experience
-                </button>
-            </div>
-        </div>
-    )
-}
-
-
-
-function InutFields() {
-    return (
-        <div className=''>
-            <div className=''>
 
                 <div className='  d-flex justify-content-between flex-wrap mt-4'>
                     <div className='col-5'>
                         <h6>Job Title</h6>
-                        <input type="text" placeholder='Mern stack developwmt ...' style={{ width: '100%' }} />
+                        <input onChange={getdata} name='title' value={data.title} type="text" placeholder='Mern stack developwmt ...' style={{ width: '100%' }} />
+                        {eror.title && <p className='text-danger'>*{eror.title.message}</p>}
                     </div>
                     <div className='col-5'>
                         <h6>Office name</h6>
-                        <input type="text" placeholder='karachi ' style={{ width: '100%' }} />
+                        <input onChange={getdata} name='office' value={data.office} type="text" placeholder='karachi ' style={{ width: '100%' }} />
+                        {eror.office && <p className='text-danger'>*{eror.office.message}</p>}
                     </div>
                     <div className='col-5 mt-4'>
                         <h6>Address</h6>
-                        <input type="text" placeholder='karachi ' style={{ width: '100%' }} />
+                        <input onChange={getdata} value={data.address} name='address' type="text" placeholder='karachi ' style={{ width: '100%' }} />
+                        {eror.address && <p className='text-danger'>*{eror.address.message}</p>}
                     </div>
                     <div className='col-5 mt-4 d-flex justify-content-between'>
                         <div className='col-5'>
                             <h6>Starting Date</h6>
-                            <input type="date" style={{ width: '100%' }} />
+                            <input onChange={getdata} name='startingDate' value={data.startingDate} type="date" style={{ width: '100%' }} />
+                            {eror.startingDate && <p className='text-danger'>*{eror.startingDate.message}</p>}
                         </div>
                         <div className='col-5'>
                             <h6>End Date</h6>
-                            <input type="date" style={{ width: '100%' }} />
+                            <input onChange={getdata} type="date" name='endDate' value={data.endDate} style={{ width: '100%' }} />
+                            {eror.endDate && <p className='text-danger'>*{eror.endDate.message}</p>}
                         </div>
                     </div>
 
                 </div>
+                <div className={` ${style.main} mt-4  text-center mt-3  `}>
+                    <button onClick={add} className={` btn py-3 mt-4  px-3 shadow-lg`}>
+                        Add
+                    </button>
+                </div>
+            </div>
+            <div className={`mt-5 ${style.main} text-center  `}>
+                <button onClick={() => navigate('/makeResume/skills')} className='btn py-3  px-3 shadow-lg'>
+                    Next to: Experience
+                </button>
             </div>
         </div>
     )
--- src/Components/BuildingResumeSession/Skills/Skills.jsx
+++ src/Components/BuildingResumeSession/Skills/Skills.jsx
@@ -4,25 +4,71 @@ import { IoIosAddCircle } from "react-icons/io";
 import { useState } from 'react';
 import { MdOutlineDeleteOutline } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+
+
 
 
 
 export default function Skills() {
-    const navigate = useNavigate()
-    const [fields, setFields] = useState([]);
-    const addField = () => {
-        setFields([...fields, <InutFields />])
+    const id = localStorage.getItem('userid');
+    const [eror, seteror] = useState({})
+
+    const [data, setdata] = useState({
+        title: '',
+        rate: '',
+        description: ''
+    })
+
+    const getdata = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setdata({
+            ...data,
+            [name]: value
+        })
     }
 
-    const deleteField = (i) => {
-        const newFields = [...fields];
-        newFields.splice(i, 1);
-        setFields(newFields);
-    };
 
-    const experience = () => {
-        navigate('/makeResume/finalize')
+    const add = async () => {
+        const url = import.meta.env.VITE_FETCHING_URL;
+        try {
+            await fetch(`${url}/addskill/${id}`, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            }).then(async (res) => {
+                const fnal = await res.json();
+                console.log(fnal);
+                if (fnal.message == 'validation eror') {
+                    seteror(fnal.eror.errors)
+                } else {
+                    if(fnal.message == 'data can not be exceed more than 5'){
+                        toast.error(fnal.message)
+                    }else{
+                        if (fnal.message == 'added') {
+                            toast.success('skill is added')
+                            seteror({});
+                            setdata({
+                                title: '',
+                                rate: '',
+                                description: ''
+                            });
+    
+                        }
+                    }
+                }
+
+            })
+        } catch (er) {
+            console.log(er);
+        }
     }
+
+    console.log(eror);
 
 
 
@@ -31,65 +77,42 @@ export default function Skills() {
             <h1>What skills would you like to highlight?</h1>
             <h4>Choose from our pre-written examples below or write your own. Tell us your experirnce where do you skills </h4>
             <div className=''>
-                <h4>Skill 1</h4>
-                <InutFields />
-            </div>
-            {fields.map((val, i) => {
-                return (
-
-                    <div className='mt-4' key={i}>
-                        <div className='d-flex justify-content-between col-5 mt-5 pt-4'>
-                            <h4>Skill {i + 2}</h4>
-                            <MdOutlineDeleteOutline onClick={() => deleteField(i)} className='fs-1 text-danger' />
-                        </div>
-                        {val}</div>
-                )
-            })}
-            <div onClick={addField} className='d-flex col-2  align-items-center border bg-primary text-white  btn  mt-5 '>
-                Add More
-                <IoIosAddCircle className='fs-2 ms-2 ' />
-
-            </div>
-
-            <div className={`mt-5 ${style.main} text-center  `}>
-                <button onClick={experience} className='btn py-3  px-3 shadow-lg'>
-                    Next to: FInalize
-                </button>
-            </div>
-        </div>
-    )
-}
-
-
-
-function InutFields() {
-    return (
-        <div className=''>
-            <div className=''>
 
                 <div className='  d-flex justify-content-between flex-wrap mt-4'>
                     <div className='col-5'>
                         <h6>Skill name</h6>
-                        <input type="text" placeholder='Mern stack developwmt ...' style={{ width: '100%' }} />
+                        <input name='title' value={data.title} onChange={getdata} type="text" placeholder='Mern stack developwmt ...' style={{ width: '100%' }} />
+                        {eror.title && <p className='text-danger'>*{eror.title.message}</p>}
                     </div>
                     <div className='col-5'>
                         <h6>Rate your skill</h6>
-                        <select name="" id="" style={{ width: '100%' }}>
-                            <option  selected disabled>Select</option>
+                        <select value={data.rate} name="rate" onChange={getdata} style={{ width: '100%' }}>
+                            <option selected disabled>Select</option>
                             <option value="25%">25%</option>
                             <option value="50%">50%</option>
                             <option value="75%">75%</option>
                             <option value="100%">100%</option>
                         </select>
-                      
+                        {eror.rate && <p className='text-danger'>*{eror.rate.message}</p>}
                     </div>
                     <div className='col-5 mt-4'>
                         <h6>About Skill</h6>
-                        <textarea rows={5} style={{ width: '100%' }} />
+                        <textarea name='description' value={data.description} onChange={getdata} rows={5} style={{ width: '100%' }} />
+                        {eror.description && <p className='text-danger'>*{eror.description.message}</p>}
                     </div>
-                   
+
 
                 </div>
+                <div className={` ${style.main} mt-4  text-center mt-3  `}>
+                    <button onClick={add} className={` btn py-3 mt-4  px-3 shadow-lg`}>
+                        Add
+                    </button>
+                </div>
+            </div>
+            <div className={`mt-5 ${style.main} text-center  `}>
+                <button className='btn py-3  px-3 shadow-lg'>
+                    Next to: FInalize
+                </button>
             </div>
         </div>
     )
