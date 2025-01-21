47d66998a44467788f95e7d5fdc5d132da9e2244
Muhammad Yousaf <yousafva9@gmail.com>
2025-01-21 15:20:40 +0500

    updated

--- src/Components/BuildingResumeSession/MainPart.jsx
+++ src/Components/BuildingResumeSession/MainPart.jsx
@@ -43,14 +43,14 @@ export default function MainPart() {
   console.log(isMdScreen);
   
   return (
-    < div className='position-relative' style={{height:'100vh'}}>
+    < div >
       {spiner && <Spinner />}
       <div className='d-md-flex'>
        {isMdScreen &&
         <Sidebar />
        }
       
-        <div  style={{ marginLeft: isMdScreen && "20%", height:'100%' }}>
+        <div className='position-relative' style={{ marginLeft: isMdScreen && "20%", height:'100vh' }}>
 
           {element}
         </div>
